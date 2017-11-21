a1feee4c52e34bc2f32daf917798295c0cb25e62
Gustavo <gustavocaraujo@gmail.com>
2017-11-21 09:24:01 -0200

    soilicitando argumentos via GET

--- 04-jQuery-alura-typer-inicial/public/js/frase.js
+++ 04-jQuery-alura-typer-inicial/public/js/frase.js
@@ -1,4 +1,11 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);function buscaFrase() {
+
+//fique a vontade de usar console.log()
+
+$("#spinner").toggle();
+var fraseId = $("#frase-id").val();
+}
 
 function fraseAleatoria() {
     $("#spinner").toggle();
@@ -9,7 +16,7 @@ function fraseAleatoria() {
             $("#erro").toggle();
         },1500);
     })
-    .always(function(){ // novo, escondendo o spinner
+    .always(function(){
         $("#spinner").toggle();
     });
 }
@@ -21,4 +28,36 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+
+function buscaFrase() {
+
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    //criacao do objeto JS que guarda a id
+    var dados = {id : fraseId};
+
+    //passando objecto como segundo parametro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+    },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+
+    console.log(data);
+
+    var frase = $(".frase");
+    frase.text(data.texto); //cuidado, texto com "o" no final
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
